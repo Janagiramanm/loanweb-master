49a0fe45c05e3aeb3de4612c763e60cbf435fce6
Janagiramanm <janagiraman@netiapps.com>
2021-03-02 22:22:43 +0530

    api debug

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Hash;
 use App\User;
 
 class UserController extends Controller
