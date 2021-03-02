37e66e805226de0efd2057e6d16f7b092c53975c
Janagiramanm <janagiraman@netiapps.com>
2021-03-02 22:11:40 +0530

    debug

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -11,39 +11,72 @@ class UserController extends Controller
 {
     public $successStatus = 200;
 
-    public function login(Request $request)
-    {
-       
-        $request->validate([ 
-            'email' => 'required|string|email',
-            'password' => 'required|string',
+    public function login(Request $request) {
+        $this->validate($request, [
+            'email' => 'required',
+            'password' => 'required'
         ]);
-        $credentials = request(['email', 'password']);
-        if(!Auth::attempt($credentials)){
-            return response()->json(['error' => 'Unauthorized'], 401);
+
+        // finding user
+        $user = User::where('email', '=', $request->input('email'))
+            ->where('primary_role', '=', 'admin')
+            ->first();
+
+        if (!$user) {
+            return response()->json([
+                'error' => 'Invalid Email'
+            ],401);
         }
-        $user = $request->user();
-        
-        //$tokenResult = $user->createToken('Personal Access Token');
+
+        // if user exist check for password
+        if (!Hash::check($request->input('password'), $user->password)) {
+            return response()->json([
+                'error' => 'Password doesn\'t match'
+            ],401);
+        }
+
         $token = $user->createToken('access_token')->accessToken;
 
-      //  $token = $tokenResult->token;
-        echo '<pre>';
-        print_r($token);
-        exit;
-       
-        if ($request->remember_me){
-            $token->expires_at = Carbon::now()->addWeeks(1);
-        }
-        $token->save();
-
-        $success['email']       =  $user->email;
-        $success['name']        =  $user->name;
-        $success['user_id']     =  $user->id;
-        $success['token']       =  $tokenResult->accessToken;
-        $success['expires_at']  =  Carbon::parse( $tokenResult->token->expires_at )->toDateTimeString();
-        return response()->json(['success' => $success], $this->successStatus);
+        return response()->json([
+            'message' => 'Authentication successful',
+            'token' => $token,
+            'user' => $user
+        ],200);
     }
+
+    // public function login(Request $request)
+    // {
+       
+    //     $request->validate([ 
+    //         'email' => 'required|string|email',
+    //         'password' => 'required|string',
+    //     ]);
+    //     $credentials = request(['email', 'password']);
+    //     if(!Auth::attempt($credentials)){
+    //         return response()->json(['error' => 'Unauthorized'], 401);
+    //     }
+    //     $user = $request->user();
+        
+    //     //$tokenResult = $user->createToken('Personal Access Token');
+    //     $token = $user->createToken('access_token')->accessToken;
+
+    //   //  $token = $tokenResult->token;
+    //     echo '<pre>';
+    //     print_r($token);
+    //     exit;
+       
+    //     if ($request->remember_me){
+    //         $token->expires_at = Carbon::now()->addWeeks(1);
+    //     }
+    //     $token->save();
+
+    //     $success['email']       =  $user->email;
+    //     $success['name']        =  $user->name;
+    //     $success['user_id']     =  $user->id;
+    //     $success['token']       =  $tokenResult->accessToken;
+    //     $success['expires_at']  =  Carbon::parse( $tokenResult->token->expires_at )->toDateTimeString();
+    //     return response()->json(['success' => $success], $this->successStatus);
+    // }
 
     public function logout(Request $request)
     {
