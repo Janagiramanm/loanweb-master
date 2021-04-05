012c6844b8d8531bb58688802627f36a26f705e8
Janagiramanm <janagiraman@netiapps.com>
2021-04-05 16:06:23 +0530

    login api count passed

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Hash;
 use App\User;
-
+use App\Model\Appointment;
 class UserController extends Controller
 {
     public $successStatus = 200;
@@ -25,6 +25,7 @@ class UserController extends Controller
 
         if (!$user) {
             return response()->json([
+                'status'=>0,
                 'error' => 'Invalid Email'
             ],401);
         }
@@ -32,27 +33,50 @@ class UserController extends Controller
         // if user exist check for password
         if (!Hash::check($request->input('password'), $user->password)) {
             return response()->json([
+                'status'=>0,
                 'error' => 'Password doesn\'t match'
             ],401);
         }
 
-        // echo '<pre>';
-        // print_r($user);
-       // $token = 'oo';
+       
+        $date = date('Y-m-d');
         $token = $user->createToken('Personal Access Token')->accessToken;
-        // print_r($token);
-        //exit;
-        // $success['email']       =  $user->email;
-        // $success['name']        =  $user->name;
-        // $success['user_id']     =  $user->id;
-        // $success['token']       =  $tokenResult->accessToken;
-        // $success['expires_at']  =  Carbon::parse( $tokenResult->token->expires_at )->toDateTimeString();
-        // return response()->json(['success' => $success], $this->successStatus);
+     
+        $new_appointment_count = Appointment::where('agent_id','=',$user->id)
+        ->where('appointment_date','<=',strtotime($date) )
+        ->count();
 
+        $pending_docs_ldfsl = Appointment::where('agent_id','=',$user->id)
+        ->where('appointmenttype_id','=','2')->count();
+
+        $bank_visit = Appointment::where('agent_id','=',$user->id)
+        ->where('appointmenttype_id','=','3')->count();
+
+        $pending_docs_bank = Appointment::where('agent_id','=',$user->id)
+        ->where('appointmenttype_id','=','4')->count();
+
+        $disbursment_docs_collection = Appointment::where('agent_id','=',$user->id)
+        ->where('appointmenttype_id','=','5')->count();
+
+        $pending_disbursment_docs = Appointment::where('agent_id','=',$user->id)
+        ->where('appointmenttype_id','=','6')->count();
+
+        $submit_demand = Appointment::where('agent_id','=',$user->id)
+        ->where('appointmenttype_id','=','7')->count();
+      
         return response()->json([
+            'status'=>1,
             'message' => 'Authentication successful',
             'token' => $token,
-            'user' => $user
+            'user' => $user,
+            'new_appointment' => $new_appointment_count,
+            'pending_docs_ldfsl' => $pending_docs_ldfsl,
+            'bank_visit' => $bank_visit,
+            'pending_docs_bank'=> $pending_docs_bank,
+            'disbursment_docs_collection'=> $disbursment_docs_collection,
+            'pending_disbursment_docs' => $pending_disbursment_docs,
+            'submit_demand' => $submit_demand
+
         ],200);
     }
 
