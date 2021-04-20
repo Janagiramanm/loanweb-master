c050be12e624e8685761c81eac1d2ee876d13e12
Janagiramanm <janagiraman@netiapps.com>
2021-04-20 11:35:58 +0530

    debug

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -242,7 +242,7 @@ class ApiController extends Controller
             }
             $msg =[
                 'status' => 1,
-                'data' => array($result)
+                'data' => $result
             ];
             return response()->json($msg);
         }
