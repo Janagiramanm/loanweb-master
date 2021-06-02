e760352ec53eae1de1dc147779b1e9c2171a3c3e
Janagiramanm <janagiraman@netiapps.com>
2021-06-02 20:28:13 +0530

    debug

--- resources/views/back-office/eligibilities/index.blade.php
+++ resources/views/back-office/eligibilities/index.blade.php
@@ -201,7 +201,7 @@
 
 			$(".form-control-select2").select2({
 				placeholder: "Select",
-				allowClear: true 
+				allowClear: true  
 			});
 			 
 			$(".occupationType").on("select2:select", function (e) {
