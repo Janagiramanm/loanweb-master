4a9b20a37f90afa85552b274c9117609c93d4c23
Janagiramanm <janagiraman@netiapps.com>
2021-06-22 22:38:16 +0530

    debug

--- resources/views/back-office/customers/newlead.blade.php
+++ resources/views/back-office/customers/newlead.blade.php
@@ -132,7 +132,7 @@
 
 
 @section('custom-script')
-<!-- <script src="{{ asset('admin/global_assets/js/demo_pages/datatables_advanced.js') }}"></script> -->
+<script src="{{ asset('admin/global_assets/js/demo_pages/datatables_advanced.js') }}"></script>
     <!-- <script src="{{ asset('admin/global_assets/js/demo_pages/datatables_basic.js') }}"></script> -->
     <script>
     $(document).ready(function() {
