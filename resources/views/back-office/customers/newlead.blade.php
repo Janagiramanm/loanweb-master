024622ad0a22ea050d865c8aaf26159ff03179a1
Janagiramanm <janagiraman@netiapps.com>
2021-06-22 22:39:15 +0530

    debug

--- resources/views/back-office/customers/newlead.blade.php
+++ resources/views/back-office/customers/newlead.blade.php
@@ -25,7 +25,7 @@
                 </div>
             </div>
 
-            <table class="table " id="newleads">
+            <table class="table datatable-pagination">
                 <thead>
                 <tr>
                     <th>#</th>
@@ -133,7 +133,7 @@
 
 @section('custom-script')
 <script src="{{ asset('admin/global_assets/js/demo_pages/datatables_advanced.js') }}"></script>
-    <!-- <script src="{{ asset('admin/global_assets/js/demo_pages/datatables_basic.js') }}"></script> -->
+    <script src="{{ asset('admin/global_assets/js/demo_pages/datatables_basic.js') }}"></script>
     <script>
     $(document).ready(function() {
         $("#submit_exce_form_btn").on("click",function() {
@@ -158,9 +158,7 @@
          $('#submit_delete_form_btn').click(function(){
             $("#submit_delte_form").submit();
          })
-        //  $('#newleads').dataTable( {
-        //     "autoWidth": false
-        // });
+        
     })
 
     </script>
