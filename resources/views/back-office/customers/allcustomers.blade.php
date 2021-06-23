570879b233ef3ebd8ac6bfcaa2fddd83487eac9e
Janagiramanm <janagiraman@netiapps.com>
2021-06-23 17:18:39 +0530

    debug

--- resources/views/back-office/customers/allcustomers.blade.php
+++ resources/views/back-office/customers/allcustomers.blade.php
@@ -24,8 +24,10 @@
                         <th>Name</th>
                         <th>E-Mail</th>
                         <th>Phone</th>
+                        <th>Telecaller</th>
+                        <th>Project Name</th>
                         <th>Status</th>
-                        
+                        <th class="text-center">Actions</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -35,8 +37,38 @@
                             <td>{{ $customer->cust_name }}</td>
                             <td>{{ $customer->cust_email }}</td>
                             <td>{{ $customer->cust_phone }}</td>
-                            <td>{{ $customer_status[$customer->application_status] }}</td>
+                            <td>{{ $customer->telecallername }}</td>
+                            <td>
+                             @php
+                             $project = App\Model\Builder::where('id','=',$customer->project_name)->first();
+                             if(isset($project->project_name)){
+                                echo $project->project_name;    
+                             }
                            
+                             @endphp
+                             </td>
+                            <td>{{ $customer_status[$customer->application_status] }}</td>
+                            <td class="text-center">
+                                <div class="list-icons">
+                                    <div class="dropdown">
+                                        <a href="#" class="list-icons-item" data-toggle="dropdown">
+                                            <i class="icon-menu9"></i>
+                                        </a>
+                                        <div class="dropdown-menu dropdown-menu-right">
+                                            <!-- <a href=""  class="dropdown-item"><i class="icon-pencil"></i> Edit </a> -->
+
+
+                                            <a class="dropdown-item" onclick="event.preventDefault();  document.getElementById('delete-form-{{ $customer->id }}').submit();">
+                                                <i class="icon-bin"></i><span>Remove</span>
+                                            </a>
+                                            <form id="delete-form-{{ $customer->id }}" action="" method="POST" style="display: none;">
+                                                @csrf @method('delete')
+
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                            </td>
                         </tr>
                     @endforeach
                 </tbody>
